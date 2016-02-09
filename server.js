eefc31c445c7241ff5dae7f5de603a3a56b1ba17
James Youn <jamesyoun710@gmail.com>
2016-02-08 19:37:30 -0800

    Add deployment files

--- server.js
+++ server.js
@@ -7,14 +7,24 @@ var app     = express();
 app.get('/', function(req, res){
   request({
     method: 'GET',
-    url: 'https://9gag.com'
+    url: 'https://github.com/showcases'
   }, function(err, response, body){
-    if(err) return console.error(err);
-    // console.log(body);
-    res.end(body);
+    if (err) return console.error(err);
+
+    // Tell Cherrio to load the HTML
+    $ = cheerio.load(body);
+    var result = [];
+    $('li.collection-card').each(function() {
+      var href = $('a.collection-card-image', this).attr('href');
+      if (href.lastIndexOf('/') > 0) {
+        result.push($('h3', this).text());
+      }
+    });
+    res.send(result);
   });
 });
 
-app.listen('3000');
-console.log('Listening on port 3000');
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log(port);
 exports = module.exports = app;
